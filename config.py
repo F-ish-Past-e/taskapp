c37447cacb57b6922b504b4c2d45b04bc5cc6b8f
F-ish-Past-e <fishingpaste@gmail.com>
2024-02-16 17:01:00 +0200

    changed db from mariadb to sqlite3

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 class Config(object):
 	APP_NAME='Task App'
 	SECRET_KEY = os.urandom(24)
-	SQLALCHEMY_DATABASE_URI = "mariadb+pymysql://the_db_username:the_db_password@localhost/the_db_you_created"
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///task_app.db'
